d312a5bafa7aca07bc82a5f2907217db2969dc6d
Khahenash <carl.goubeau@gmail.com>
2014-10-28 16:29:26 +0100

    correctifs profiles + utilisation cross_val

--- usefullTools.r
+++ usefullTools.r
@@ -101,7 +101,7 @@ learnVal <- function (dataFt, dataTarg,trainId, valId, nbN, nbLoop){
 
 cross_val <- function (dataFt, dataTarg, nbN, nbLoop, fold){
   # mélange des indices
-  ind <- sample(1:dim(dataFt[1]),dim(dataFt[1]))
+  ind <- sample(1:dim(dataFt)[1],dim(dataFt)[1])
   score <- NULL
   
   for(i in 1:fold){
@@ -115,4 +115,65 @@ cross_val <- function (dataFt, dataTarg, nbN, nbLoop, fold){
     par(fg = "red")
     lines(res$it, res$mseV, type = "l")
   }
+}
+
+
+compute_profiles <- function (symbol, nr=5, nc=3){
+  left_profile <- matrix(data=0, nrow = 1, ncol = nr)
+  right_profile <- matrix(data=0, nrow = 1, ncol = nr)
+  top_profile <- NULL
+  bottom_profile <- NULL
+  
+  for(row in nr:1){
+    found_left <- F
+    best_right_elem <- 0
+    
+    col <- 1
+    while(col <= nc){
+      if(symbol[(row-1)*nc+col] == 1 && !found_left){
+        found_left <- T
+        left_profile[row] <- ((row-1)*nc+col)
+      }
+      if(symbol[(row-1)*nc+col] == 1){
+        best_right_elem <- ((row-1)*nc+col)
+      }
+      col <- col +1
+    }
+    right_profile[row] <- best_right_elem
+  }
+  
+  return (list(left=left_profile, right=right_profile))
+}
+
+compute_centroid <- function (symbol, nr=5, nc=3){
+  nb_pts <- 0
+  sum_col <- 0
+  sum_row <- 0
+  
+  for(row in 1:nr){
+    for(col in 1:nc){
+      if(symbol[(row-1)*nc+col] == 1){
+        sum_row <- sum_row + row
+        sum_col <- sum_col + col
+        nb_pts <- nb_pts +1
+      }
+    }
+  }
+  
+  return (list(row=round(sum_row/nb_pts), col=round(sum_col/nb_pts)))
+}
+
+
+compute_img <- function(symbol, nr, nc){
+  img <- NULL
+  for(i in 1:(nr*nc)){
+    if(i %in% symbol){
+      img <- c(img, 1)
+    }
+    else{
+      img <- c(img, 0)
+    }
+  }
+  
+  return (img)
 }
